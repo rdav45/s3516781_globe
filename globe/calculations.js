02f3a9fa327ae3b47c49242b5513f6b94d8891c8
Bernie Jenny <jennyb@geo.oregonstate.edu>
2017-04-20 21:27:22 +1000

    Auto-formatted HTML and JS files

--- globe/calculations.js
+++ globe/calculations.js
@@ -1,112 +1,112 @@
 window.onload = initCalculations;
 
 function initCalculations() {
-	var pointsOrig = [new LatLon(89.9, 0), new LatLon(-20, 0), new LatLon(20, -40), new LatLon(-90, 40)];
-	var points = [new LatLon(89.9, 0), new LatLon(40, 0), new LatLon(-20, 0), new LatLon(89, 0)];//[];
-	//refinement
-	/*for(var i = 0; i < pointsOrig.length-1; i++){
-		points.push(pointsOrig[i]);
-		var dist = pointsOrig[i].distanceTo(pointsOrig[i+1]);
-		if(dist > 20){
-			var eps = Math.floor(dist/10);
-			console.log(eps);
-			for(var j = 1; j < eps; j++){
-				var brng = pointsOrig[i].bearingTo(pointsOrig[i+1]);
-				points.push(pointsOrig[i].destinationPoint(brng, j*10));
-			}
-		}
-		
-	}*/
-	//points.push(pointsOrig[pointsOrig.length-1]);
-		
-	var pointsA = [];
-	var pointsB = [];
-	var width = 10;
-	//startPoint
-	var brng = points[0].bearingTo(points[1]);
-	pointsA.push(points[0].destinationPoint(brng+90, width));
-	pointsB.push(points[0].destinationPoint(brng-90, width));
-	
-	//betweenPoints
-	for(var i = 1; i < points.length-1; i++){
-		var brng1 = points[i].bearingTo(points[i+1]);
-		var brng2 = points[i-1].bearingTo(points[i]);
-		var brng = (brng1+brng2)/2;
-		pointsA.push(points[i].destinationPoint(brng+90, width));
-		pointsB.push(points[i].destinationPoint(brng-90, width));	
-	}
-	                                                
-	//endPoint
-	var brng = points[points.length-2].finalBearingTo(points[points.length-1]);
-	pointsA.push(points[points.length-1].destinationPoint(brng+90, width));
-	pointsB.push(points[points.length-1].destinationPoint(brng-90, width));
-	
-	var c=document.getElementById("myCanvas");
-	var ctx=c.getContext("2d");
-	
-	ctx.beginPath();
-	/*
-	ctx.moveTo(lonToCanvas(points[0].lon()), latToCanvas(points[0].lat()));
-	for(var i = 1; i < points.length; i++){
-		ctx.lineTo(lonToCanvas(points[i].lon()), latToCanvas(points[i].lat()));	
-	}*/
-	var phinew = ((pointsA[0].lat() * 180/Math.PI) / 2) + (Math.PI/4);
-	ctx.moveTo(lonToCanvas(pointsA[0].lon(), phinew), latToCanvas(pointsA[0].lon(), phinew));
-	for(var i = 1; i < pointsA.length; i++){
-		var phinew = ((pointsA[i].lat() * 180/Math.PI) / 2) + (Math.PI/4);
-		ctx.lineTo(lonToCanvas(pointsA[i].lon(), phinew), latToCanvas(pointsA[i].lon(), phinew));	
-	}
-	var phinew = ((pointsB[pointsB.length-1].lat() * 180/Math.PI) / 2) + (Math.PI/4);
-	ctx.lineTo(lonToCanvas(pointsB[pointsB.length-1].lon(), phinew), latToCanvas(pointsB[pointsB.length-1].lon(), phinew));
-	for(var i = pointsB.length-2; i >= 0; i--){
-		var phinew = ((pointsB[i].lat() * 180/Math.PI) / 2) + (Math.PI/4);
-		ctx.lineTo(lonToCanvas(pointsB[i].lon(), phinew), latToCanvas(pointsB[i].lon(), phinew));	
-	}
-/*
-	ctx.lineTo(lonToCanvas(pointsB[0].lon()), latToCanvas(pointsB[0].lat()));
-	for(var i = 1; i < pointsB.length; i++){
-		ctx.lineTo(lonToCanvas(pointsB[i].lon()), latToCanvas(pointsB[i].lat()));	
-	}*/
-	
-	/*
-	ctx.moveTo(lonToCanvas(p1.lon()), latToCanvas(p1.lat()));
-	ctx.lineTo(lonToCanvas(p2.lon()), latToCanvas(p2.lat()));
-	
-	ctx.moveTo(lonToCanvas(p1.lon()), latToCanvas(p1.lat()));
-	ctx.lineTo(lonToCanvas(p1A.lon()), latToCanvas(p1A.lat()));
-	ctx.moveTo(lonToCanvas(p1.lon()), latToCanvas(p1.lat()));
-	ctx.lineTo(lonToCanvas(p1B.lon()), latToCanvas(p1B.lat()));
-	
-	ctx.moveTo(lonToCanvas(p2.lon()), latToCanvas(p2.lat()));
-	ctx.lineTo(lonToCanvas(p2A.lon()), latToCanvas(p2A.lat()));
-	ctx.moveTo(lonToCanvas(p2.lon()), latToCanvas(p2.lat()));
-	ctx.lineTo(lonToCanvas(p2B.lon()), latToCanvas(p2B.lat()));*/
-	
-	
-	ctx.fillStyle = '#f00';
-	
-	ctx.closePath();
-	ctx.fill();
+    var pointsOrig = [new LatLon(89.9, 0), new LatLon(-20, 0), new LatLon(20, -40), new LatLon(-90, 40)];
+    var points = [new LatLon(89.9, 0), new LatLon(40, 0), new LatLon(-20, 0), new LatLon(89, 0)];//[];
+    //refinement
+    /*for(var i = 0; i < pointsOrig.length-1; i++){
+     points.push(pointsOrig[i]);
+     var dist = pointsOrig[i].distanceTo(pointsOrig[i+1]);
+     if(dist > 20){
+     var eps = Math.floor(dist/10);
+     console.log(eps);
+     for(var j = 1; j < eps; j++){
+     var brng = pointsOrig[i].bearingTo(pointsOrig[i+1]);
+     points.push(pointsOrig[i].destinationPoint(brng, j*10));
+     }
+     }
+     
+     }*/
+    //points.push(pointsOrig[pointsOrig.length-1]);
 
-	
-	//ctx.stroke();
-	
-	
+    var pointsA = [];
+    var pointsB = [];
+    var width = 10;
+    //startPoint
+    var brng = points[0].bearingTo(points[1]);
+    pointsA.push(points[0].destinationPoint(brng + 90, width));
+    pointsB.push(points[0].destinationPoint(brng - 90, width));
+
+    //betweenPoints
+    for (var i = 1; i < points.length - 1; i++) {
+        var brng1 = points[i].bearingTo(points[i + 1]);
+        var brng2 = points[i - 1].bearingTo(points[i]);
+        var brng = (brng1 + brng2) / 2;
+        pointsA.push(points[i].destinationPoint(brng + 90, width));
+        pointsB.push(points[i].destinationPoint(brng - 90, width));
+    }
+
+    //endPoint
+    var brng = points[points.length - 2].finalBearingTo(points[points.length - 1]);
+    pointsA.push(points[points.length - 1].destinationPoint(brng + 90, width));
+    pointsB.push(points[points.length - 1].destinationPoint(brng - 90, width));
+
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+    ctx.beginPath();
+    /*
+     ctx.moveTo(lonToCanvas(points[0].lon()), latToCanvas(points[0].lat()));
+     for(var i = 1; i < points.length; i++){
+     ctx.lineTo(lonToCanvas(points[i].lon()), latToCanvas(points[i].lat()));	
+     }*/
+    var phinew = ((pointsA[0].lat() * 180 / Math.PI) / 2) + (Math.PI / 4);
+    ctx.moveTo(lonToCanvas(pointsA[0].lon(), phinew), latToCanvas(pointsA[0].lon(), phinew));
+    for (var i = 1; i < pointsA.length; i++) {
+        var phinew = ((pointsA[i].lat() * 180 / Math.PI) / 2) + (Math.PI / 4);
+        ctx.lineTo(lonToCanvas(pointsA[i].lon(), phinew), latToCanvas(pointsA[i].lon(), phinew));
+    }
+    var phinew = ((pointsB[pointsB.length - 1].lat() * 180 / Math.PI) / 2) + (Math.PI / 4);
+    ctx.lineTo(lonToCanvas(pointsB[pointsB.length - 1].lon(), phinew), latToCanvas(pointsB[pointsB.length - 1].lon(), phinew));
+    for (var i = pointsB.length - 2; i >= 0; i--) {
+        var phinew = ((pointsB[i].lat() * 180 / Math.PI) / 2) + (Math.PI / 4);
+        ctx.lineTo(lonToCanvas(pointsB[i].lon(), phinew), latToCanvas(pointsB[i].lon(), phinew));
+    }
+    /*
+     ctx.lineTo(lonToCanvas(pointsB[0].lon()), latToCanvas(pointsB[0].lat()));
+     for(var i = 1; i < pointsB.length; i++){
+     ctx.lineTo(lonToCanvas(pointsB[i].lon()), latToCanvas(pointsB[i].lat()));	
+     }*/
+
+    /*
+     ctx.moveTo(lonToCanvas(p1.lon()), latToCanvas(p1.lat()));
+     ctx.lineTo(lonToCanvas(p2.lon()), latToCanvas(p2.lat()));
+     
+     ctx.moveTo(lonToCanvas(p1.lon()), latToCanvas(p1.lat()));
+     ctx.lineTo(lonToCanvas(p1A.lon()), latToCanvas(p1A.lat()));
+     ctx.moveTo(lonToCanvas(p1.lon()), latToCanvas(p1.lat()));
+     ctx.lineTo(lonToCanvas(p1B.lon()), latToCanvas(p1B.lat()));
+     
+     ctx.moveTo(lonToCanvas(p2.lon()), latToCanvas(p2.lat()));
+     ctx.lineTo(lonToCanvas(p2A.lon()), latToCanvas(p2A.lat()));
+     ctx.moveTo(lonToCanvas(p2.lon()), latToCanvas(p2.lat()));
+     ctx.lineTo(lonToCanvas(p2B.lon()), latToCanvas(p2B.lat()));*/
+
+
+    ctx.fillStyle = '#f00';
+
+    ctx.closePath();
+    ctx.fill();
+
+
+    //ctx.stroke();
+
+
 }
 //x
-function lonToCanvas(lon, phinew){
-	var x = 2 * Math.sin(phinew) * Math.sin(lon);
-	//console.log("x: ", x);
-	//return (x*90) + 250;
+function lonToCanvas(lon, phinew) {
+    var x = 2 * Math.sin(phinew) * Math.sin(lon);
+    //console.log("x: ", x);
+    //return (x*90) + 250;
 }
 //y
-function latToCanvas(lon, phinew){
-	var y = 2 * Math.sin(phinew) * Math.cos(lon);
-	//console.log("y: ", y);
-	//return (y*90) + 250;
+function latToCanvas(lon, phinew) {
+    var y = 2 * Math.sin(phinew) * Math.cos(lon);
+    //console.log("y: ", y);
+    //return (y*90) + 250;
 }
-    
-    
-   
+
+
+
 
 
--- globe/src/webgl.js
+++ globe/src/webgl.js
@@ -1,16 +1,16 @@
-    function initGLContext(canvas) {
-        try {
-            gl = canvas.getContext("experimental-webgl", { antialias: true});
-            gl.viewportWidth = canvas.width;
-            gl.viewportHeight = canvas.height;
-        } catch (e) {
-        }
-        if (!gl) {
-            alert("Could not initialise WebGL");
-        }
+function initGLContext(canvas) {
+    try {
+        gl = canvas.getContext("experimental-webgl", {antialias: true});
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+    } catch (e) {
     }
-	
-	
+    if (!gl) {
+        alert("Could not initialise WebGL");
+    }
+}
+
+
 //from: http://webreflection.blogspot.com/2010/09/fragment-and-vertex-shaders-my-way-to.html
 
 // our shaders base path
@@ -18,39 +18,39 @@ loadShaders.base = "shader/";
 
 // our shaders loader (getShader)
 function loadShaders(gl, shaders, callback) {
-	// (C) WebReflection - Mit Style License
-	function onreadystatechange() {
-		var xhr = this, i = xhr.i;
-		if (xhr.readyState == 4) {
-			shaders[i] = gl.createShader(shaders[i].slice(0, 2) == "fs" ? gl.FRAGMENT_SHADER : gl.VERTEX_SHADER);
-			gl.shaderSource(shaders[i], xhr.responseText);
-			gl.compileShader(shaders[i]);
-			if (!gl.getShaderParameter(shaders[i], gl.COMPILE_STATUS))
-				throw gl.getShaderInfoLog(shaders[i]);
-			!--length && typeof callback == "function" && callback(shaders);
-		}
-	}
+    // (C) WebReflection - Mit Style License
+    function onreadystatechange() {
+        var xhr = this, i = xhr.i;
+        if (xhr.readyState == 4) {
+            shaders[i] = gl.createShader(shaders[i].slice(0, 2) == "fs" ? gl.FRAGMENT_SHADER : gl.VERTEX_SHADER);
+            gl.shaderSource(shaders[i], xhr.responseText);
+            gl.compileShader(shaders[i]);
+            if (!gl.getShaderParameter(shaders[i], gl.COMPILE_STATUS))
+                throw gl.getShaderInfoLog(shaders[i]);
+            !--length && typeof callback == "function" && callback(shaders);
+        }
+    }
 
-	for (var shaders = [].concat(shaders), asynchronous = !!callback, i = shaders.length, length = i, xhr; i--; ) {
-		( xhr = new XMLHttpRequest).i = i;
-		xhr.open("get", loadShaders.base + shaders[i], asynchronous);
-		if (asynchronous) {
-			xhr.onreadystatechange = onreadystatechange;
-		}
-		xhr.send(null);
-		onreadystatechange.call(xhr);
-	}
-	return shaders;
+    for (var shaders = [].concat(shaders), asynchronous = !!callback, i = shaders.length, length = i, xhr; i--; ) {
+        (xhr = new XMLHttpRequest).i = i;
+        xhr.open("get", loadShaders.base + shaders[i], asynchronous);
+        if (asynchronous) {
+            xhr.onreadystatechange = onreadystatechange;
+        }
+        xhr.send(null);
+        onreadystatechange.call(xhr);
+    }
+    return shaders;
 }
 
 
-function setupProgram(program){
-	gl.useProgram(program);
-	if (program === rasterProgram) {
+function setupProgram(program) {
+    gl.useProgram(program);
+    if (program === rasterProgram) {
         // texture
         gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
-		
-		program.vertexPositionAttribute = gl.getAttribLocation(program, "aVertexPosition");
+
+        program.vertexPositionAttribute = gl.getAttribLocation(program, "aVertexPosition");
         gl.enableVertexAttribArray(program.vertexPositionAttribute);
 
         //shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
@@ -58,26 +58,26 @@ function setupProgram(program){
 
         program.samplerUniform = gl.getUniformLocation(program, "uSampler");
         program.shiftUniform = gl.getUniformLocation(program, "uShift");
-		
+
     } else if (program === vectorProgram) {
         // color
         program.vertexPositionAttribute = gl.getAttribLocation(program, "aVertexPosition");
         gl.enableVertexAttribArray(program.vertexPositionAttribute);
-		/*
-		program.vertexPositionAttribute_n1 = gl.getAttribLocation(program, "aVertexPosition_n1");
-        gl.enableVertexAttribArray(program.vertexPositionAttribute_n1);
-		
-		program.vertexPositionAttribute_n2 = gl.getAttribLocation(program, "aVertexPosition_n2");
-        gl.enableVertexAttribArray(program.vertexPositionAttribute_n2);*/
+        /*
+         program.vertexPositionAttribute_n1 = gl.getAttribLocation(program, "aVertexPosition_n1");
+         gl.enableVertexAttribArray(program.vertexPositionAttribute_n1);
+         
+         program.vertexPositionAttribute_n2 = gl.getAttribLocation(program, "aVertexPosition_n2");
+         gl.enableVertexAttribArray(program.vertexPositionAttribute_n2);*/
 
-       // program.vertexColorAttribute = gl.getAttribLocation(program, "aVertexColor");
+        // program.vertexColorAttribute = gl.getAttribLocation(program, "aVertexColor");
         //gl.enableVertexAttribArray(program.vertexColorAttribute);
     }
-	
-	program.pMatrixUniform = gl.getUniformLocation(program, "uPMatrix");
+
+    program.pMatrixUniform = gl.getUniformLocation(program, "uPMatrix");
     program.mvMatrixUniform = gl.getUniformLocation(program, "uMVMatrix");
-	program.meridianRotationUniform = gl.getUniformLocation(program, "uMeridianRotation");
-    program.sinLatPoleUniform = gl.getUniformLocation(program, "uSinLatPole");        
+    program.meridianRotationUniform = gl.getUniformLocation(program, "uMeridianRotation");
+    program.sinLatPoleUniform = gl.getUniformLocation(program, "uSinLatPole");
     program.cosLatPoleUniform = gl.getUniformLocation(program, "uCosLatPole");
     program.viewportHeightUniform = gl.getUniformLocation(program, "uViewportHeight");
     program.radiusUniform = gl.getUniformLocation(program, "uRadius");
@@ -87,14 +87,14 @@ function setupProgram(program){
 /**
  * Provides requestAnimationFrame in a cross browser way.
  */
-window.requestAnimFrame = (function() {
-  return window.requestAnimationFrame ||
-         window.webkitRequestAnimationFrame ||
-         window.mozRequestAnimationFrame ||
-         window.oRequestAnimationFrame ||
-         window.msRequestAnimationFrame ||
-         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-           window.setTimeout(callback, 1000/60);
-         };
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+                window.setTimeout(callback, 1000 / 60);
+            };
 })();
 
